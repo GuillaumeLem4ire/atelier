bceb5c472c3a11a6764ab7718f17cabc293e668d
GuillaumeLem4ire <47924772+GuillaumeLem4ire@users.noreply.github.com>
2019-02-23 18:22:01 +0100

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,304 @@
+// écouteur de l'évènement deviceready de Cordova
+document.addEventListener('deviceready', onDeviceReady, false);
+
+// fonction de callback de l'évènement deviceready
+function onDeviceReady() {
+
+    // on utilise le UI framework Framework7 (v3) - framework7.io
+    // **********************************************************
+
+    // initialisation de Framework7
+    var f7App = new Framework7({
+        root: '#app',
+        // routes
+        routes: [
+            {
+                name: 'details',
+                path: '/details/:title',
+                url: './pages/details.html'
+            }
+            ,
+            {
+                name: 'add',
+                path: '/add/',
+                url: './pages/add.html'
+            }
+        ]
+    });
+    
+    var searchbar = f7App.searchbar.create({
+        
+        el: '.searchbar',
+        searchContainer:'.list',
+        searchIn : '.swipeout-content a'
+        
+        
+    })
+
+    var mainView = f7App.views.create('.view-main');
+
+    var NOTEAPP = {
+
+        // initialisation
+        // **************
+        init: function(){
+            // enregistrement des écouteurs
+            this.bindings();
+            // affichage de la liste des notes mémorisées
+            this.displayNotes();
+        },
+
+        // récupère les notes présentes dans localStorage
+        // et retourne un objet JSON (objet vide si aucune note)
+        // *****************************************************
+        getNotes: function(){
+            // on récupère les notes éventuelles sauvegardées dans localStorage
+            // (objet JSON)
+            // si la clé 'note++' existe dans localStorage et n'est pas vide...
+            // (c-à-d si au moins une note est sauvegardée)
+            if(localStorage['note++'] != undefined && localStorage['note++'] != ''){
+                // on désérialise l'objet JSON stocké
+                var objNotes = JSON.parse(localStorage['note++']);
+            }
+            // s'il n'y a aucune note sauvegardée
+            else{
+                var objNotes = {};
+            };
+            return objNotes;
+        },
+
+        // affiche les notes actuellement présentes dans localStorage
+        // dans la List View de la page home
+        // **********************************************************
+        displayNotes: function(){
+
+            // on retire tous les éléments éventuellement présents dans la List View
+            // nb: on retire tous les éléments de classe .note afin de ne pas retirer
+            // l'élément de classe .template qui doit exister pour pouvoir être dupliqué
+            // lorsque l'on ajoute une note à la List View
+            $('#memoList li.note').remove();
+
+            // on récupère les notes éventuelles sauvegardées dans localStorage
+            // (objet JSON - appel à la méthode getNotes()
+            var objNotes = this.getNotes();
+
+            // si l'objet n'est pas vide
+            if(! $.isEmptyObject(objNotes)){
+                // on parcourt toutes les notes stockées dans l'objet JSON
+                // rappel: boucle Javascript for...in parcourt toutes les propriétés d'un objet
+                for(n in objNotes){
+                    // on ajoute la note à la liste
+                    // ****************************
+
+                    // on clone le template
+                    var newItem = $('#memoList li.template')
+                        .clone()
+                        .removeClass('template')
+                        // pour différentier un item 'normal' du template
+                        .addClass('note')
+                        .show();
+
+                    // on donne comme texte à l'élément de liste le tite de la note
+                    $('.swipeout-content a', newItem).text(n);
+
+                    // on ajoute le nouvel élément à la liste
+                    $('#memoList').append(newItem);
+
+                };
+                // on cache l'élément 'aucun mémo'
+                $('#noMemo').hide();
+            }
+            else{
+                // on affiche l'élément 'aucun mémo'
+                // car on n'a aucune note à afficher
+                $('#noMemo').show();
+            }
+
+        },
+
+        // ajoute une note dans localStorage
+        // *********************************
+        addNote: function(title, memo) {
+            // on récupère les notes sauvegardées dans le localStorage
+            var objNotes = this.getNotes();
+
+            // on ajoute une note à l'objet JSON
+            objNotes[title] = memo;
+
+            // on sauve la version sérialisée de l'objet modifié dans localStorage
+            localStorage['note++'] = JSON.stringify(objNotes);
+        },
+
+        // efface une note dans localStorage
+        // *********************************
+        deleteNote: function(title){
+            // on récupère toutes les notes stockées dans localStorage
+            // (appel à getNotes())
+                var objNotes = this.getNotes();
+            // on efface la note sélectionnée dans l'objet JSON
+                delete objNotes[title];
+            // on sérialise l'objet JSON modifié et on le sauvegarde dans
+            // localStorage
+                localStorage['note++']=JSON.stringify(objNotes);
+        },
+
+        // gestionnaires d'évènements
+        // **************************
+        bindings: function(){
+
+            // on sauve le contexte de l'application
+            // *************************************
+            var _noteapp = this;
+
+            // gestionnaire de l'évènement click sur le bouton Ajouter
+            // *******************************************************
+            $('.convert-form-to-data').on('click', function(){
+
+                // on récupère les données du formulaire
+                var formData = f7App.form.convertToData('#addForm');
+                var title = formData.title;
+                var memo = formData.memo;
+
+                // si le titre de la note n'est pas vide
+                if(title != ''){
+                    // on stocke la note
+                    _noteapp.addNote(title, memo);
+                    // on affiche les notes
+                    
+                    _noteapp.displayNotes();
+
+                    // on vide les deux inputs
+                    $("input[name='title']").val('');
+                    $("input[name='memo']").val('');
+
+                }
+
+            })
+
+            // gestionnaire de l'évènement click sur un lien de la liste des notes
+            // *******************************************************************
+            $(document).on('click', '#memoList li .swipeout-content a', function(){
+
+                // on récupère le titre de la note (texte de l'élément cliqué)
+                var title = $(this).text();
+
+                // on navique vers la page de détail
+                // en transmettant le titre comme paramètre
+                mainView.router.navigate({
+                    name: 'details',
+                    params: {title: title}
+                })
+
+            })
+            
+            $(document).on('click', '#addBtn', function(){
+
+            
+
+                // on navique vers la page de détail
+                // en transmettant le titre comme paramètre
+                mainView.router.navigate({
+                    name: 'add'
+                })
+
+            })
+
+            // gestionaire de l'évènement page:init sur la page de détails
+            // ***********************************************************
+            // (son DOM est prêt à être manipulé)
+            $(document).on('page:init', '.page[data-name="details"]', function(e){
+
+                // on récupère la page (voir aide F7 http://framework7.io/docs/page.html)
+                var page = e.detail;
+
+                // on récupère le paramètre title (voir aide F7)
+                var title = decodeURIComponent(page.route.params.title);
+
+                // on récupère l'élément racine de la page de détail
+                // (voir aide F7)
+                var pageDom = page.el;
+
+                // on récupère le contenu de localStorage (toutes les notes stockées)
+                var objNotes = _noteapp.getNotes();
+
+                // on récupère le contenu de la note
+                var memo = objNotes[title];
+
+
+                // on injecte title et memo dans le DOM de la page de détail
+                $('.page-content .title', pageDom).text(title);
+                $('.page-content .memo', pageDom).text(memo);
+
+            })
+            
+            //gestionnaire de l'ajout de note dans la page ADD
+            // ****************************************************************************
+            
+            $(document).on('page:init', '.page[data-name="add"]', function(e){
+			$('.convert-form-to-data').on('click', function(){
+
+                // on récupère les données du formulaire
+                var formData = f7App.form.convertToData('#addForm');
+                var title = formData.title;
+                var memo = formData.memo;
+
+                // si le titre de la note n'est pas vide
+                if(title != ''){
+                    // on stocke la note
+                    _noteapp.addNote(title, memo);
+                    // on affiche les notes
+                    
+                    mainView.router.back();
+            
+                    _noteapp.displayNotes();
+
+                    // on vide les deux inputs
+                    $("input[name='title']").val('');
+                    $("input[name='memo']").val('');
+
+                }
+
+            })
+            })
+            
+
+            // gestionnaire de l'évènement click sur le bouton Effacer de la page de détail
+            // ****************************************************************************
+            $(document).on('click', "[data-name='details'] #deleteBtn", function(){
+            
+            var title=$("[data-name='details'] .page-content .title").text();
+            
+            _noteapp.deleteNote(title);
+            
+             mainView.router.back();
+            
+            _noteapp.displayNotes();
+
+            }) // Fin bouton effacer de la page détail
+          
+            
+    // gestionnaire du swipeout
+    // ***************************************************************************		
+		$(document).on('swipeout:deleted', '.swipeout', function(){
+            
+            var title=$(".swipeout-content a", this).text();
+            
+			console.log(title);
+            
+            _noteapp.deleteNote(title);
+            
+           // mainView.router.back();
+            
+            //_noteapp.displayNotes();
+            
+            
+		  }) //Fin Swipeout
+        } // Fin Bindings
+    } // Fin NOTEAPP
+
+    // Initialisation de l'application
+    // *******************************
+    NOTEAPP.init();
+    
+
+}
